ea3eed98a18a60d20db9e3a6ec1e459812347570
philipng88 <philiplyng@gmail.com>
2019-10-25 17:54:48 -0600

    added functionality to calculate average tour rating and to prevent duplicate reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -40,7 +40,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating cannot be below 1.0'],
-      max: [5, 'Rating cannot be above 5.0']
+      max: [5, 'Rating cannot be above 5.0'],
+      set: val => Math.round(val * 10) / 10
     },
     ratingsQuantity: {
       type: Number,
@@ -118,6 +119,9 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
+
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
